67a5cc946567972658878c7b9e99ba0e6f82efc9
jopers <noudiba@gmail.com>
2026-07-17 11:20:10 +0100

    Update top_10k_players.py

--- top_10k_players.py
+++ top_10k_players.py
@@ -51,12 +51,12 @@ def save_to_db(data_list):
     cursor.execute(f'''
         CREATE TABLE IF NOT EXISTS elite_stats (
             player_id INTEGER PRIMARY KEY,
-            name TEXT,
+            web_name TEXT,
             team TEXT,
-            global_selection_pct REAL,
-            elite_starter_selection_pct REAL,
-            current_price REAL,
-            price_change_event REAL,
+            element_type INTEGER,
+            global_selected REAL,
+            Top_10k_selected REAL,
+            now_cost REAL,
             last_gw_points INTEGER,
             last_gw_minutes INTEGER
         )
@@ -89,10 +89,10 @@ def main():
             "player_id": p_id,
             "web_name": p['web_name'],
             "team": teams_map[p['team']],
+            "element_type": p['element_type'],
             "global_selected": float(p['selected_by_percent']),
             "Top_10k_selected": 0, # سيحسب لاحقاً
             "now_cost": p['now_cost'] / 10,
-            "cost_change_event": p['cost_change_event'] / 10,
             "last_gw_points": live_data.get(p_id, {}).get('total_points', 0),
             "last_gw_minutes": live_data.get(p_id, {}).get('minutes', 0),
             "_internal_count": 0 # مؤقت للحساب
@@ -114,7 +114,7 @@ def main():
     # 6. الحساب النهائي للنسبة المئوية وتجهيز القائمة النهائية
     final_data = []
     for p_id, data in players_info.items():
-        data['elite_starter_selection_pct'] = round((data['_internal_count'] / TOP_MANAGERS_COUNT) * 100, 2)
+        data['Top_10k_selected'] = round((data['_internal_count'] / TOP_MANAGERS_COUNT) * 100, 2)
         # حذف العداد الداخلي قبل الحفظ
         final_info = {k: v for k, v in data.items() if k != "_internal_count"}
         final_data.append(final_info)
